2f4713a0c267f35b3afdaed186b688e6ab7bb719
Jacob Viti <thevitij64@gmail.com>
2020-08-04 01:26:20 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_writer
+  attr_reader :name, :species
+
+  @@all = []
+
+def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self
+end
+
 end
