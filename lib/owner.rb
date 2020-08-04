1dd2183a2f78f3cb37c359893066699b0a63159b
Jacob Viti <thevitij64@gmail.com>
2020-08-04 02:46:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
  end
 
  def list_pets
- return "I have #{@owner.dogs} dog(s), and #{Cat.all.count} cat(s)."
+ return "I have #{self.dogs} dog(s), and #{Cat.all.count} cat(s)."
  end
 
 end
